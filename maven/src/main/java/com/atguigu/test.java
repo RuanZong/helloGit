ae33fbdaa20fb4920f50b039288d90250a03cfd7
wangyu <1337651356@qq.com>
2020-10-27 21:45:48 +0800

    分支修改

--- maven/src/main/java/com/atguigu/test.java
+++ maven/src/main/java/com/atguigu/test.java
@@ -5,6 +5,6 @@ public class test {
         System.out.println("hello");
         System.out.println("更新一");
         System.out.println("更新二");
-        System.out.println("分支添加");
+        System.out.println("主干修改");
     }
 }

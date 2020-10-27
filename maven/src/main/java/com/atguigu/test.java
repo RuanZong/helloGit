bf04400039f64427c806f44f06ded9c45e2e6430
wangyu <1337651356@qq.com>
2020-10-27 22:01:45 +0800

    本地分支修改

--- maven/src/main/java/com/atguigu/test.java
+++ maven/src/main/java/com/atguigu/test.java
@@ -6,6 +6,7 @@ public class test {
         System.out.println("更新一");
         System.out.println("更新二");
         System.out.println("分支修改");
+        System.out.println("本地分支修改");
         System.out.println("远程分支修改");
     }
 }
